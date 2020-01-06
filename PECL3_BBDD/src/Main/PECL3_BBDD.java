f9f267cf6ba29e5deb04e84d1024380b623dfee1
RazvanVC <razvanvc@yahoo.com>
2020-01-06 04:07:23 +0100

    Organizacion e Insert TIENDA

---
+++ PECL3_BBDD/src/Main/PECL3_BBDD.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author razvanvc, albertumus, IreneGarciaMoreno
+ */
+public class PECL3_BBDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Iniciar_Sesion nt = new Iniciar_Sesion();
+        nt.setVisible(true);
+    }
+    
+}
