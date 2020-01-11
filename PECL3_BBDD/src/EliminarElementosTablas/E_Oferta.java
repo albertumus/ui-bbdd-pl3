79d7f62458b2df1d836d1790a51a6b827abf2539
RazvanVC <razvanvc@yahoo.com>
2020-01-11 07:00:07 +0100

    Mod ECupon y EOferta

--- PECL3_BBDD/src/EliminarElementosTablas/E_Oferta.java
+++ PECL3_BBDD/src/EliminarElementosTablas/E_Oferta.java
@@ -5,13 +5,10 @@
  */
 package EliminarElementosTablas;
 
-import MenusTablas.M_Tienda;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -142,20 +139,18 @@ public class E_Oferta extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(E_Oferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(E_Oferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(E_Oferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(E_Oferta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @SuppressWarnings("override")
             public void run() {
                 new E_Oferta(conexion).setVisible(true);
             }
